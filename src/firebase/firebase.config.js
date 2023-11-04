ff88443141d5e56c9499d8eb90e86048a666cc73
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-11-04 20:09:30 +0600

    setup firebase and install some important item

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwRdzdXEPTBYS1QcO6fhk_8ubh-xXWpGI",
+  authDomain: "career-maker-assignment-11.firebaseapp.com",
+  projectId: "career-maker-assignment-11",
+  storageBucket: "career-maker-assignment-11.appspot.com",
+  messagingSenderId: "755116808734",
+  appId: "1:755116808734:web:4b2b5dad3eccdbad1fac0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
