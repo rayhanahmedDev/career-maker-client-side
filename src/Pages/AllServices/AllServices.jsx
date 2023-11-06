0045ba84073f6497fa5db68d0f3dc577a5c8fcd6
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-11-06 11:01:14 +0600

    implement search functionality by the service name

--- src/Pages/AllServices/AllServices.jsx
+++ src/Pages/AllServices/AllServices.jsx
@@ -14,11 +14,20 @@ const AllServices = () => {
             .then(data => setAllService(data))
     }, [])
 
+    const handleSearch = ()=>{
+        const inputField = document.getElementById('input-field')
+        const inputValue = inputField.value.toLowerCase()
+        const card = allServices
+        const result = card.filter(card => card.ServiceName.toLowerCase() === inputValue)
+        setAllService(result)
+    }
+
     return (
         <div>
             <div className="text-center my-12">
-                <input type="text" placeholder="Type here" className="input input-bordered input-error w-full max-w-xs" />
-                <button className="btn bg-gradient-to-r from-[#FF3300] to-[#FF8938] text-white">Search</button>
+                {/* input field */}
+                <input type="text" id="input-field" placeholder="Type here" className="input input-bordered input-error w-full max-w-xs" />
+                <button onClick={handleSearch} className="btn bg-gradient-to-r from-[#FF3300] to-[#FF8938] text-white">Search</button>
             </div>
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-12">
                 {
@@ -26,7 +35,7 @@ const AllServices = () => {
                 }
             </div>
             <div className="text-center my-12">
-                <div className={dataLength === allServices.length && 'hidden'}>
+                <div className={dataLength === allServices.length ? 'hidden' : undefined}>
                     <button onClick={() => setDataLength(allServices.length)} className="btn bg-gradient-to-r from-[#FF3300] to-[#FF8938] text-white">More</button>
                 </div>
             </div>
